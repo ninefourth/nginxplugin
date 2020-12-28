2707b7e3cd9288ae2711e77a1b33e49e47d04d8c
ninefourth <33535597+ninefourth@users.noreply.github.com>
2020-12-28 18:10:17 +0800

    build

--- build.sh
+++ build.sh
@@ -1,32 +1,16 @@
-#download
+#nginx version you need
 version=1.14.0
-echo ============download nginx code.
+
 cd /usr/local/nginxplugin
 cd nginx-$version
 
-echo ============apply patch
-
-#git apply ../ngx_healthcheck_module/nginx_healthcheck_for_nginx_1.14+.patch
-
-echo ===========begin build nginx
-
 make clean
-
-#./configure --with-debug --with-stream --add-module=../ngx_healthcheck_module --with-http_ssl_module --with-http_realip_module
-#./configure --with-debug --with-stream --with-http_ssl_module --with-http_realip_module  --add-module=../ngx_xfdf_ip_hash_module --add-module=../ngx_healthcheck_module
-
-#./configure --with-stream --with-http_ssl_module --with-http_realip_module  --add-module=../ngx_xfdf_ip_hash_module --add-module=../ngx_healthcheck_module --add-module=../ngx_http_dyups_module
 ./configure --with-stream  --with-http_ssl_module --with-http_realip_module --add-module=../ngx_xfdf_ip_hash_module --add-module=../ngx_healthcheck_module --add-module=../ngx_http_endpoint_module
 #--add-module=../ngx_xfdf_ip_hash_module
 #--add-module=../ngx_http_endpoint_module
 # --add-module=../ngx_healthcheck_module
- #--add-module=../ngx_http_dyups_module
-
-#./configure --with-debug --with-stream --with-http_ssl_module --with-http_realip_module  --add-module=../ngx_healthcheck_module
-#./configure --with-debug --with-stream --with-http_ssl_module --with-http_realip_module  --add-module=../ngx_xfdf_ip_hash_module
 
 make
 sudo make install
 
-echo ===========start nginx
 echo finish!
